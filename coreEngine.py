e41559949bc724e51fa4f04f447097c537a0e2da
Patrick Cohen <patrick.cohen.at@gmail.com>
2017-05-05 18:59:59 -0700

    Specify rules for finding time of impact Add function to check if certain channel is available

--- coreEngine.py
+++ coreEngine.py
@@ -1,13 +1,26 @@
 import os
 import os.path
 import csv
-import numpy
+import numpy as np
 from numpy import genfromtxt
 
 
 # define empty class for data structure:
 class Data:
 	pass
+
+def checkChannelAvail(channel):
+	# dismiss all non-numbers ( NaN):
+	numValues = np.extract(np.logical_not(np.isnan(channel)), channel)
+	finiteValues = np.extract(np.isfinite(numValues), numValues)
+	nonZeros = np.extract(np.nonzero(finiteValues), finiteValues)
+	# print ("Numvalues: ", numValues)
+	# print ("finiteValues: ", finiteValues)
+	# print ("nonZeros: ", nonZeros)
+	if len(nonZeros)>0:
+		return True
+	else:
+		return False
 
 # define vechicle class:
 class Vehicle(object):
@@ -22,11 +35,56 @@ class Vehicle(object):
 	def loadData(self):
 		fileName = os.getcwd() + '/'+ self.VIN + '.csv'
 		if (os.path.isfile(fileName)):
+			self.data.isavailable = True
 			self.data.raw = genfromtxt(fileName, delimiter=',')
 			self.data.time = self.data.raw[1:,0]
 			self.data.speedometer = self.data.raw[1:,0]
+			self.data.parkingSensor_rear = self.data.raw[1:,24]
 		else:
-			print("No data available for ", VIN )
+			self.data.isavailable = False
+			# print("No data available for ", VIN )
+
+	def establishImpactTime(self):
+		# check if parking sensor data is available:
+		avail = checkChannelAvail(self.data.parkingSensor_rear)
+
+		if(avail):
+			print("Parking sensor data is available")
+
+		
+
+		# if(parking distance sensor signal is available):
+		# 	find time at which the indicated distance of the first sensor becomes (very close to) zero.
+		# elif (brakeOn/Off signal is available):
+		# 	if(brake ==0):
+		# 		find time at which deceleration becomes greater that possible from coast-down
+		# 		(need to consider gradient of road??)
+		# 	else:
+		# 		if not binary:
+		# 			find time at which deceleration becomes greater than what is caused by braking	==> simple vehicle dynamics model (based on historic data)
+		# 		else:
+		# 			if (deceleration greater than full braking would caused)
+
+		# 		else:
+		# 			Fallback solution
+		
+		
+		# elif high impact:
+		# 	find time at which abs(acel_X) becomes greater than what could be achieved by braking.
+
+		# 	check against (RELEVANT impact accelerometer data is available):
+		# 	find time at which accel from impact sensor starts to diverge from accel of vehicle.
+		# 	only relevant if large deformations in crash structure present (sensor sits on crash-beam)
+			
+
+		# else:
+		# 	Fallback solution:
+		# 	analyse shape of acceleration (more gradual if caused by braking)
+		# 	if pitch angle available: 
+		# 			if deceleration > pitch angle indicates ==> assumed impact (requires simple vehicle model)
+		# 		is yaw rate relevant?
+
+		# 	over time, a ML model could learn to identify time of impact from accelerometer data (training data is data that has clear signal (e.g. paking sensor))
 
 # define accident class:
 class Accident(object):
@@ -38,24 +96,19 @@ class Accident(object):
 	def runAnalysis(self):
 		if(self.category == "ParkingLot"):
 			print("running analysis")
-			# establishImpactTime()
+			for vehicle in accident.involvedVehicles:
+				if (vehicle.data.isavailable ==True):
+					print(vehicle.VIN)
+					vehicle.establishImpactTime()
+
+
+
 		else:
 			print("unable to analyse this type of accident")
 
-	# def establishImpactTime():
-		if(parking distance sensor signal is available):
-			find time at which the indicated distance of the first sensor becomes (very close to) zero.
-		elif (RELEVANT impact acceleromer data is available):
-			find time at which accel from impact sensor starts to diverge from accel of vehicle.
-		elif (brakeOn/Off signal is available)
-			find time at which deceleration becomes greater that possible from coast-down
-			(need to consider gradient of road??)
-		else:
-			if(mid to high speed impact): 
-				find time at which abs(acel_X) becomes greater than what could be achieved by braking.
-			else:
-				??????????
+	
 
+		
 
 
 
@@ -71,12 +124,14 @@ car2.loadData()
 accident = Accident([car1, car2], "ParkingLot")
 
 
-for parties in accident.involvedVehicles:
-	print(parties.VIN)
-	print(parties.data.time)
+for party in accident.involvedVehicles:
+	# print("VIN: ",party.VIN)
+	# print("Time: ",party.data.time)
+	# print("Speedometer: ",party.data.speedometer)
+	# print("Parking Sensor Rear: ",party.data.parkingSensor_rear)
 
 
-accident.runAnalysis()
+	accident.runAnalysis()
 
 
 
