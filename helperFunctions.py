5255565a6075bf78c86d553d136a17ffe9e004e3
Patrick Cohen <patrick.cohen.at@gmail.com>
2017-05-07 10:23:17 -0700

    Make lowpass filter parameters variable

--- helperFunctions.py
+++ helperFunctions.py
@@ -5,8 +5,8 @@ from scipy import signal
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-def lowPass(channel):
-	b, a = signal.butter(8, 0.125)
+def lowPass(channel, order=2, f_crit=0.25):
+	b, a = signal.butter(order, f_crit)
 	y = signal.filtfilt(b, a, channel, padlen=21)
 
 	return y
